06ca4c234b879529e6c6628d9a6639a6936de70a
adi <paswan.adi619@gmail.com>
2026-09-24 19:52:12 +1000

    design the form page

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace TaskFlow
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            cmbCategory = new ComboBox();
+            btnadd = new Button();
+            btnDelete = new Button();
+            lstTasks = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(106, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Title";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(286, 63);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(211, 27);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "txtTitle";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(109, 162);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Category";
+            // 
+            // cmbCategory
+            // 
+            cmbCategory.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbCategory.FormattingEnabled = true;
+            cmbCategory.Items.AddRange(new object[] { "Study", "Work" });
+            cmbCategory.Location = new Point(326, 159);
+            cmbCategory.Name = "cmbCategory";
+            cmbCategory.Size = new Size(151, 28);
+            cmbCategory.TabIndex = 3;
+            // 
+            // btnadd
+            // 
+            btnadd.Location = new Point(362, 269);
+            btnadd.Name = "btnadd";
+            btnadd.Size = new Size(94, 29);
+            btnadd.TabIndex = 4;
+            btnadd.Text = "Add";
+            btnadd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(545, 269);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 5;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // lstTasks
+            // 
+            lstTasks.FormattingEnabled = true;
+            lstTasks.Location = new Point(327, 334);
+            lstTasks.Name = "lstTasks";
+            lstTasks.Size = new Size(150, 104);
+            lstTasks.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lstTasks);
+            Controls.Add(btnDelete);
+            Controls.Add(btnadd);
+            Controls.Add(cmbCategory);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "TaskFlow";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private ComboBox cmbCategory;
+        public Button btnadd;
+        private Button btnDelete;
+        private ListBox lstTasks;
+    }
+}
